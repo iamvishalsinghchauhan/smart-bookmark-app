50395e31cee16bc21c864972de498d769271f704
70937_Vishal <vschauhan.work@gmail.com>
2026-02-14 00:15:14 +0530

    Smart Bookmark App

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { supabase } from "../lib/supabaseClient";
+import { supabase } from "../../lib/supabaseClient";
 import { useEffect, useState } from "react";
 import type { User, Session } from "@supabase/supabase-js";
 
